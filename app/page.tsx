4f6b9abf7627311252eb5b4e45be107e2ca5040b
thatmehdude <anaelechisom16@gmail.com>
2025-12-16 13:39:44 +0100

    update globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Page () {
   return (
     <section>
-      <h1 className="text-center">The hub for every dev</h1>
+      <h1 className="text-center">The hub for every dev <br /> event you can't miss</h1>
+      <p className="text-center mt-5">Hackatons, meetups, and conferences, all in one place</p>
     </section>
   )
 }
