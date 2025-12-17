dbcfd5735a5cb22b8622300ab57073f52b53c77f
thatmehdude <anaelechisom16@gmail.com>
2025-12-17 16:01:39 +0100

    add type exports for database

--- database/index.ts
+++ database/index.ts
@@ -2,3 +2,6 @@ import Event from './event.model';
 import Booking from './booking.model';
 
 export { Event, Booking };
+
+export type {IEvent} from './event.model';
+export type {IBooking} from './booking.model';
