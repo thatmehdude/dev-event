dc7f4962aa84e8f79533b0b1fb21d050ac0b8a21
thatmehdude <anaelechisom16@gmail.com>
2025-12-17 12:37:06 +0100

    create event card, update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
+import { events } from "@/lib/constants";
 
 export default function Page () {
   return (
@@ -10,8 +12,10 @@ export default function Page () {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
         <ul className="events list-none">
-          {[1, 2, 3, 4, 5].map((event) => (
-            <li key={event}>Event {event}</li>
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event}/>
+            </li>
           ))}
         </ul>
       </div>
